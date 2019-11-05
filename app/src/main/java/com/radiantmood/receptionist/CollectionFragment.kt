a3f5a2f735dfbd5564b7e8cfc4e46e11dc43b847
Atom Raiff <atom.raiff@gmail.com>
2019-11-04 23:13:44 -0800

    init

---
+++ app/src/main/java/com/radiantmood/receptionist/CollectionFragment.kt
@@ -0,0 +1,32 @@
+package com.radiantmood.receptionist
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CollectionFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CollectionFragment()
+    }
+
+    private lateinit var viewModel: CollectionViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.collection_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CollectionViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/radiantmood/receptionist/CollectionViewModel.kt
@@ -0,0 +1,7 @@
+package com.radiantmood.receptionist
+
+import androidx.lifecycle.ViewModel
+
+class CollectionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
